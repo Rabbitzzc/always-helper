94f21f480be25af7e4ec61e3e24bc9ba3c9b5212
Rabbitzzc <zzclovelcs@gmail.com>
2020-10-13 17:34:11 +0800

    fix: 🐛 replaceClass 参数问题修复

--- types/style/replaceClass.d.ts
+++ types/style/replaceClass.d.ts
@@ -2,5 +2,5 @@
  * 将元素指定class替换为另外的class
  * 兼容性：https://caniuse.com/?search=classList
  */
-declare const replaceClass: (el: HTMLElement | string, oldClassName: string, newClassName: string, className: string) => void;
+declare const replaceClass: (el: HTMLElement | string, oldClassName: string, newClassName: string) => void;
 export default replaceClass;
